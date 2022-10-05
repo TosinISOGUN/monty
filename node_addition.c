54248c12beb3d6e56dd3939b44bee8da498ee802
Biswealth1 <abisolaa282@gmail.com>
2022-10-04 23:45:17 -0700

    Update solution to tasks

---
+++ node_addition.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+* addnode - function that adds node to the head stack
+* @head: head of the stack
+* @n: new value
+*
+* Return: nothing
+*/
+void addnode(stack_t **head, int n)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (temp)
+		temp->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
